18eab66436f67fc3b4e3a8c8b6088c38538e9a46
Levi Smith <itslevismith@gmail.com>
2025-11-04 16:09:26 -0500

    Fix stencils requiring pure black/white stencils and not working with runtime generated stencils

--- playdate/sprites.lua
+++ playdate/sprites.lua
@@ -10,8 +10,18 @@ module.kCollisionTypeBounce = "bounce"
 playdate.graphics.sprite = module
 
 local mask_shader = love.graphics.newShader[[
+    vec3 rgb2hsv(vec3 c)
+    {
+        vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+        vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+        vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+        float d = q.x - min(q.w, q.y);
+        float e = 1.0e-10;
+        return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+    }
    vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-      if (Texel(texture, texture_coords).rgb == vec3(0.0)) {
+      if (rgb2hsv(vec3(Texel(texture, texture_coords))).z < 0.45f) {
          // a discarded pixel wont be applied as the stencil.
          discard;
       }
